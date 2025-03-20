24ea4fb0368ee674c84d09515d86462e21ac60f5
Jericho Delos Reyes <jericho.dlsreyes@gmail.com>
2025-03-20 23:56:25 +0800

    Updated versions

--- multidimensionalArrays/main.cpp
+++ multidimensionalArrays/main.cpp
@@ -2,29 +2,57 @@
 using namespace std;
 
 int main() {
-    int cube[2][3][4];
+    int layers, rows, cols;
 
-    cout << "Enter 24 elements for the 3D array:\n";
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 3; j++) {
-            for (int k = 0; k < 4; k++) {
-                cin >> cube[i][j][k];
+    // Ask the user for the 3D array size
+    cout << "Enter number of layers: ";
+    cin >> layers;
+    cout << "Enter number of rows: ";
+    cin >> rows;
+    cout << "Enter number of columns: ";
+    cin >> cols;
+
+    // Declare a 3D array dynamically
+    int ***array = new int**[layers];
+    for (int i = 0; i < layers; i++) {
+        array[i] = new int*[rows];
+        for (int j = 0; j < rows; j++) {
+            array[i][j] = new int[cols];
+        }
+    }
+
+    // Taking input for the 3D array
+    cout << "\nEnter the elements:\n";
+    for (int i = 0; i < layers; i++) {
+        for (int j = 0; j < rows; j++) {
+            for (int k = 0; k < cols; k++) {
+                cout << "Element at [" << i << "][" << j << "][" << k << "]: ";
+                cin >> array[i][j][k];
             }
         }
     }
 
-
+    // Displaying the 3D array
     cout << "\nThe 3D array is:\n";
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < layers; i++) {
         cout << "Layer " << i << ":\n";
-        for (int j = 0; j < 3; j++) {
-            for (int k = 0; k < 4; k++) {
-                cout << cube[i][j][k] << " ";
+        for (int j = 0; j < rows; j++) {
+            for (int k = 0; k < cols; k++) {
+                cout << array[i][j][k] << " ";
             }
             cout << endl;
         }
         cout << endl;
     }
+
+    // Free dynamically allocated memory
+    for (int i = 0; i < layers; i++) {
+        for (int j = 0; j < rows; j++) {
+            delete[] array[i][j];
+        }
+        delete[] array[i];
+    }
+    delete[] array;
 
     return 0;
 }
